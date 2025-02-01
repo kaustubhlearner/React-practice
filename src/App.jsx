386dd90721cfeba087f076019d749bb45c310272
kaustubhlearner <kaustubhpandey81788@gmail.com>
2025-02-01 10:06:57 +0530

    Second Task-N

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import images from './Assets/dog.jpg'
-
+let name = (name) => {
+  alert(name)
+}
 function App() {
+  const name = (name) => {
+    alert(name)
+  }
+  
   return (
     <>
-    <h1>kaustubh Todos</h1>
-    <img src={images} alt="" style={{height:"200px"}}/>
-    <ul>
-      <li>invent new traffic light</li>
-      <li>Reharse a movie Scene</li>
-      <li>Improve the spectrum technolgy</li>
-    </ul>
-    <button>Click me !</button>
+<button onClick={()=>{name('kaustubh')}}>click me </button>
     </>
   )
 }
