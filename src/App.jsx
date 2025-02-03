ac9e9d7ddfe129252539be011a622541ee060ad4
kaustubhlearner <kaustubhpandey81788@gmail.com>
2025-02-03 09:49:27 +0530

    controlled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import { useState } from "react"
 
 function App() {
-  const [change, setchanage] = useState('apple')
-  function chnagereq() {
-    setchanage('Banana')
-  }
+  const [name, setname] = useState('')
+  const [password, setpassword] = useState('')
+  const [email, setemail] = useState('')
+
   return (
     <>
-      <h1>{change}</h1>
-      <button onClick={chnagereq}>click me</button>
+      <h1>Controller component</h1>
+      <form action="">
+        <input type="text" onChange={(event) => setname(event.target.value)} placeholder="Enter the value" />
+        <br /> <br />
+        <input type="password" onChange={(event) => setpassword(event.target.value)} placeholder="Enter the password" />
+        <br /> <br />
+        <input type="email" onChange={(event) => setemail(event.target.value)} placeholder="Enter the email" />
+        <br /> <br />
+        <button>Submit</button>
+        <h3>{name}</h3>
+        <h3>{password}</h3>
+        <h3>{email}</h3>
+        <button onClick={()=>setname('')} >clear</button> 
+        <button onClick={()=>setemail('')} >clearEmail</button> 
+        <button onClick={()=>setpassword('')} >clearPassword</button> 
+      </form>
     </>
   )
 }
