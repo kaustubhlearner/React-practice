eb4f825fa4ad38c4e5c1bc5f09745184d6220e90
kaustubhlearner <kaustubhpandey81788@gmail.com>
2025-02-04 09:45:53 +0530

    handle-checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import Checkbox from "./components/Checkbox"
 
 function App() {
   const [name, setname] = useState('')
@@ -7,22 +8,7 @@ function App() {
 
   return (
     <>
-      <h1>Controller component</h1>
-      <form action="">
-        <input type="text" onChange={(event) => setname(event.target.value)} placeholder="Enter the value" />
-        <br /> <br />
-        <input type="password" onChange={(event) => setpassword(event.target.value)} placeholder="Enter the password" />
-        <br /> <br />
-        <input type="email" onChange={(event) => setemail(event.target.value)} placeholder="Enter the email" />
-        <br /> <br />
-        <button>Submit</button>
-        <h3>{name}</h3>
-        <h3>{password}</h3>
-        <h3>{email}</h3>
-        <button onClick={()=>setname('')} >clear</button> 
-        <button onClick={()=>setemail('')} >clearEmail</button> 
-        <button onClick={()=>setpassword('')} >clearPassword</button> 
-      </form>
+  <Checkbox/>
     </>
   )
 }
---
+++ src/components/Checkbox.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+const Checkbox = () => {
+    const [skills, setskills] = useState([])
+    const handleskills = (event) => {
+        if(event.target.value){
+            setskills([...skills,event.target.value])
+        }
+
+    }
+
+return (
+    <>
+        <h1>Select Your Skills</h1>
+        <input onChange={handleskills} type="checkbox" id="PHP" value={'PHP'} />
+        <label htmlFor="PHP">Php</label>
+        <input onChange={handleskills} type="checkbox" id='Javascript' value={"Javascript"} />
+        <label htmlFor="Javascript">Javascript</label>
+        <input onChange={handleskills} type="checkbox" id="Python" value={'Python'} />
+        <label htmlFor="python">Python</label>
+        <h1>{skills.toString()}</h1>
+    </>
+)
+}
+
+export default Checkbox
