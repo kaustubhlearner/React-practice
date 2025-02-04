9fd35fd09e28cd8968eab5b308d42dfb3d74e82a
kaustubhlearner <kaustubhpandey81788@gmail.com>
2025-02-04 10:39:35 +0530

    handle-option

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -2,19 +2,31 @@ import React, { useState } from 'react'
 
 const Checkbox = () => {
     const [gender, setgender] = useState('')
- 
-return (
-    <>
-    <h1>handle radio button</h1>
-    <input type="radio" name="gender" id="male"  value={'male'} onChange={(event)=>setgender(event.target.value)}/>
-    <label htmlFor="male">Male</label>
-    <br />
-    <input type="radio" name="gender" id="female" value={'female'} onChange={(event)=>setgender(event.target.value)}/>
-    <label htmlFor="female">Female</label>
-    <br />
-    <h1>Your selected Gender : {gender}</h1>
-    </> 
-)
+    const [city, setcity] = useState('bihar')
+
+    return (
+        <>
+            <h1>handle radio button</h1>
+            <input type="radio" name="gender" id="male" value={'male'} onChange={(event) => setgender(event.target.value)} />
+            <label htmlFor="male">Male</label>
+            <br />
+            <input type="radio" name="gender" id="female" value={'female'} onChange={(event) => setgender(event.target.value)} />
+            <label htmlFor="female">Female</label>
+            <br />
+            <h1>Your selected Gender : {gender}</h1>
+            <br /><br />
+
+            <h1>select your city </h1>
+            <select defaultValue={'bihar'} onChange={(event)=>setcity(event.target.value)}>
+
+                <option value="noida" >Noida</option>
+                <option value="bihar" >bihar</option>
+                <option value="delhi" >delhi</option>
+                <option value="haryana" >haryana</option>
+            </select>
+            <h2>Selected your city: {city} </h2>
+        </>
+    )
 }
 
 export default Checkbox
