21815fb1e06bccb197e15e6eb0c3abe20f29499d
kaustubhlearner <kaustubhpandey81788@gmail.com>
2025-02-11 10:30:54 +0530

    routing-react-js

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react"
-import Checkbox from "./components/Checkbox"
+import { Routes, Route, Link } from "react-router"
+import Home from "./Home"
+import About from "./About"
+import Info from "./Info"
 
 function App() {
   return (
     <>
-
-      <Checkbox  />
-
+      <Link to='/'>Home</Link>
+      <Link to='/about'>About</Link>
+      <Link to='/info'>info</Link>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/info" element={<Info />} />
+      </Routes>
     </>
   )
 }
